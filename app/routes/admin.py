9c823c2ca039a5300ef0b4853185a9f6a31d91ca
sun12wook11 <sun12wook11@gmail.com>
2024-09-02 17:49:44 +0900

    민수님 admin 변경 적용

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -4,6 +4,9 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.templating import Jinja2Templates
 
+from app.dbfactory import get_db
+from app.model.member import Member
+
 admin_router = APIRouter()
 templates = Jinja2Templates(directory='views/templates')
 
@@ -19,8 +22,18 @@ async def mgproduct(req: Request):
 
 # 회원 관리 페이지
 @admin_router.get("/mguser", response_class=HTMLResponse)
-async def mguser(req: Request):
-    return templates.TemplateResponse("admin/mguser.html", {"request": req})
+async def mguser(req: Request, db: Session = Depends(get_db)):
+    userid = req.session.get('userid')
+    is_admin = req.session.get('is_admin')
+
+    # 관리자가 아닌 경우 접근 불가
+    if not userid or not is_admin:
+        return RedirectResponse(url='/', status_code=303)
+
+    # 모든 회원 정보를 가져옵니다.
+    member = db.query(Member).all()
+
+    return templates.TemplateResponse("admin/mguser.html", {"request": req, "member": member})
 
 # 주문 관리 페이지
 @admin_router.get("/mgorder", response_class=HTMLResponse)
