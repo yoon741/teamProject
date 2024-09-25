7743029dd61a9be8811a67e71d3aa41514e8a771
yoon <mono74140@gmail.com>
2024-09-25 16:03:30 +0900

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -3,11 +3,11 @@ from pydantic.v1 import BaseSettings
 class Settings(BaseSettings):
     userid: str = 'ubuntu'
     passwd: str = 'ubuntu'
-    # dbname: str = 'clouds2024'
-    dbname: str = 'ubuntu'
+    dbname: str = 'clouds2024'
+    #dbname: str = 'ubuntu'
     dburl: str = 'localhost'
-    #dbconn: str = f'sqlite:///app/{dbname}.db'
+    dbconn: str = f'sqlite:///app/{dbname}.db'
     #?charset=utf8mb4 이모지 추가기능
-    dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
+    #dbconn: str = f'mysql+pymysql://{userid}:{passwd}@{dburl}:3306/{dbname}?charset=utf8mb4'
 
 config = Settings()
